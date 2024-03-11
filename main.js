f7f0d56fb53fb5bdfe0280c108e02d380b2ea885
Simon Huang <huisimonliu@gmail.com>
2024-03-10 20:40:42 -0700

    Data Parsed into dictionary by year, then a list for each county

--- main.js
+++ main.js
@@ -18,61 +18,129 @@ let teamMargin = {top: 10, right: 30, bottom: 30, left: 60},
     teamHeight = height-450 - teamMargin.top - teamMargin.bottom;
 
 let heatmapMargin = {top: 10, right: 10, bottom: 10, left: 10}
-let heatmapTop = 500
-let heatmapLeft = 600
+let heatmapTop = 400
+let heatmapLeft = 400
 let heatmapWidth = 50 + heatmapLeft
 let heatmapHeight = 50 + heatmapTop
 
+const codeToCountyMap = {
+    "06001": "Alameda", 
+    "06031": "Kings",
+    "06061": "Placer",
+    "06091": "Sierra",
+    "06003": "Alpine",
+    "06033": "Lake",
+    "06063": "Plumas",
+    "06093": "Siskiyou",
+    "06005": "Amador",
+    "06035": "Lassen",
+    "06065": "Riverside",
+    '06095': "Solano",	
+    "06007": "Butte",
+    "06037": "Los Angeles",
+    "06067": "Sacramento",
+    "06097": "Sonoma",
+    "06009": "Calaveras",
+    "06039": "Madera",
+    "06069": "San Benito",
+    "06099": "Stanislaus",
+    "06011": "Colusa",
+    "06041": "Marin",
+    "06071": "San Bernardino",
+    "06101": "Sutter",
+    "06013": "Contra Costa",
+    "06043": "Mariposa",
+    "06073": "San Diego",
+    "06103": "Tehama",
+	"06015": "Del Norte",
+    "06045": "Mendocino",
+    "06075": "San Francisco",	 	
+    "06105": "Trinity",
+    "06017": "El Dorado",
+    "06047": "Merced",
+    "06077": "San Joaquin",
+    "06107": "Tulare",
+    "06019": "Fresno",
+    "06049": "Modoc",
+    "06079": "San Luis Obispo",
+    "06109": "Tuolumne",
+    "06021": "Glenn",
+    "06051": "Mono",
+    "06081": "San Mateo",
+    "06111": "Ventura",
+    "06023": "Humboldt", 
+    "06053": "Monterey",
+    "06083": "Santa Barbara",
+    "06113": "Yolo",
+    "06025": "Imperial",
+    "06055": "Napa",
+    "06085": "Santa Clara",
+    "06115": "Yuba",
+    "06027": "Inyo",
+    "06057": "Nevada",
+    "06087": "Santa Cruz",
+    "06029": "Kern",
+    "06059": "Orange",
+    "06089": "Shasta", 	
+}
 
 // read the raw data from csv to plot
-let data = d3.csv("california.csv").then (rawData => {
+d3.csv("california.csv").then (rawData => {
     let yearDict = {}
-
+    console.log(rawData)
     rawData.forEach(element => {
         if (element.FIRE_YEAR in yearDict) {
-            yearDict[element.FIRE_YEAR].push(element)
+            let countyName= codeToCountyMap[element.FIPS_CODE]
+
+            if (countyName in yearDict[element.FIRE_YEAR]) {
+                yearDict[element.FIRE_YEAR][countyName].push(element)
+            } else {
+                yearDict[element.FIRE_YEAR][countyName] = []
+            }
+
         } else {
-            yearDict[element.FIRE_YEAR] = []
+            yearDict[element.FIRE_YEAR] = {}
         }
-        
+        return
     });
-    return yearDict
+
+    console.log(yearDict)
+
+    const myColor = d3.scaleLinear()
+    .range(["white", "red"])
+    .domain([0, 100]) // Max Fire Count in dictionary
+
+    d3.json("caliCounties.geojson").then (counties => {
+        let selectedYearData = yearDict["2000"] // ToDo Adjust when using html input box selection.
+
+        console.log(counties)
+
+        // Select Our Root coordinate to build map upon
+        let county = counties.features.filter(county => county.properties.NAME === "Sacramento")[0]
+        console.log(county)
+
+        // Handle placement of points on SVG
+        let projection = d3.geoMercator().fitExtent([[heatmapLeft, heatmapTop], [heatmapWidth, heatmapHeight]], county)
+        let pathGen = d3.geoPath().projection(projection)
+
+        let svg = d3.select("svg")
+            
+        counties.features.forEach(county => {
+            svg.append('path')
+                .datum(county)
+                .attr("d", pathGen)
+                .attr('fill', 'none')
+                .attr('stroke', 'steelblue')
+                .attr('stroke-width', '2')
+
+        })
+
+    })
+
 
 }).catch(function(error){
     console.log(error);
 });
-
-console.log(data)
-
-d3.json("caliCounties.geojson").then (counties => {
-    console.log(counties)
-
-    let county = counties.features.filter(county => county.properties.NAME === "Sacramento")[0]
-    console.log(county)
-
-    let projection = d3.geoMercator().fitExtent([[heatmapLeft, heatmapTop], [heatmapWidth, heatmapHeight]], county)
-
-    let pathGen = d3.geoPath().projection(projection)
-
-    let mapPath = pathGen(county)
-    console.log(mapPath)
-
-    let svg = d3.select("svg")
-        
-    counties.features.forEach(county => {
-        svg.append('path')
-            .datum(county)
-            .attr("d", pathGen)
-            .attr('fill', 'none')
-            .attr('stroke', '#999999')
-            .attr('stroke-width', '2')
-
-    })
-
-})
-
-
-
 
 
 
